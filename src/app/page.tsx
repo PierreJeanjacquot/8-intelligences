417884903ebb513e634be5e87c393f272a2b1e90
PierreJeanjacquot <26487010+PierreJeanjacquot@users.noreply.github.com>
2025-10-08 11:24:24 +0200

    style: better space usage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,15 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center">
-      <div className="mb-8 text-center">
-        <Link href="/questionnaire">
-          <Button>Decouvre tes intelligences</Button>
-        </Link>
+    <div className="flex flex-col items-center justify-between h-full min-h-[60vh]">
+      <div className="flex-1 flex items-center justify-center">
+        <div className="text-center">
+          <Link href="/questionnaire">
+            <Button>Decouvre tes intelligences</Button>
+          </Link>
+        </div>
       </div>
-      <div>Avec</div>
+      <div className="mb-4">Avec</div>
     </div>
   );
 }
